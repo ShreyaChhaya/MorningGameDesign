d13e5b8b6a8aea84592686b96529286506afad23
ShreyaChhaya <chhayas25@greenhill.org>
2022-06-01 09:57:05 -0500

    new one

---
+++ age.py
@@ -0,0 +1,12 @@
+#Shreya Chhaya
+#Calculate age
+#get user year and current year
+import os
+os.system('cls')
+by=2007 #this is a number
+by=int(input('year you were born,'))
+currentYear=2022
+age=currentYear-by
+print('your age is', age)
+if age >50:
+    print('you are old')
