5c827bdd72e2ea4ee711e12e7963f25e8318eb3f
ShreyaChhaya <chhayas25@greenhill.org>
2022-06-01 09:37:47 -0500

    new one

---
+++ AddTwoNumbers.py
@@ -0,0 +1,18 @@
+#Shreya Chhaya 
+# Two numbers
+# We will ask user for two numbers 
+# and add them
+# anything after # is 
+# ignored by the computer
+#importthings
+import os
+os.system('cls')
+#use variables
+
+Num1=23
+Num2=12
+answer=Num1+Num2
+#operators =, -, *, /
+print('the addition of ', Num1, 'and', Num2, 'is', answer)
+print(Num1)
+print(Num2)
